ba489ae7a2c6c19283822ca7e1a559e1e56a7a4d
Lawrence Aritao <lawrence.aritao@gmail.com>
2024-04-15 23:22:13 +0000

    dockerized with sample variables

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -4,9 +4,9 @@ module.exports =  ({ env }) => ({
 		connection: {
 		host: env('DATABASE_HOST', 'localhost'),
 			port: env.int('DATABASE_PORT', 5432),
-			database: env('DATABASE_NAME', 'carla'),
-			user: env('DATABASE_USERNAME', 'carla'),
-			password: env('DATABASE_PASSWORD', 'loveCarla'),
+			database: env('DATABASE_NAME', 'strapi'),
+			user: env('DATABASE_USERNAME', 'strapi'),
+			password: env('DATABASE_PASSWORD', 'strapi123'),
 			ssl: env.bool('DATABASE_SSL', false)
 		}
 	}
